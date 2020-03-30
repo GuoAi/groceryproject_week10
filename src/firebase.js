6a90786909536b2b02a49c1ba7c46c7b267f3d0e
GuoAi <49813920+GuoAi@users.noreply.github.com>
2020-03-31 00:54:59 +0800

    Update FirebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,15 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    //Key in your database details
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: ""
-
-  };
+  apiKey: "AIzaSyDz8oeI5KTnY4dpe4cBeUf11g9tdPCIImY",
+  authDomain: "bt3103-week9-d8ec0.firebaseapp.com",
+  databaseURL: "https://bt3103-week9-d8ec0.firebaseio.com",
+  projectId: "bt3103-week9-d8ec0",
+  storageBucket: "bt3103-week9-d8ec0.appspot.com",
+  messagingSenderId: "886879645959",
+  appId: "1:886879645959:web:d9f527a94d11b03ad5d5a2",
+  measurementId: "G-4QHHP6L11H"
+};
   
   firebase.initializeApp(firebaseConfig);
   var database = firebase.firestore();
